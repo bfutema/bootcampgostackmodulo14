4d17a93c427b7e1c3ae8ef4d64fdff8355824a49
Bruno Amaral Futema <bruno.futema@outlook.com>
2020-01-12 00:29:12 -0300

    App GoBarber

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
+
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
+import App from './App';
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('5f18e41c-03a4-4ad2-9dab-3172c8bb43c0');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
